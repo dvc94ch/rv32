e704d2716f151d9f60007d16f2771ec1a5928dbe
David Craven <david@craven.ch>
2021-06-13 15:52:33 +0200

    Add test script.

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python3 -m rv32.alu
+python3 -m rv32.branch
+python3 -m rv32.core
+python3 -m rv32.decoder
+python3 -m rv32.loadstore
+python3 -m rv32.ram
+python3 -m rv32.regs
+python3 -m rv32.rom
